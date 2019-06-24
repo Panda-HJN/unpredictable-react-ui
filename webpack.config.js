04584bc1bc86d563fac2a881c021dc97471bb1e8
韩佳宁 <hanjianing@mudu.tv>
2019-06-24 18:50:46 +0800

    html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports={
     mode: 'production',
     entry:{
@@ -16,5 +17,10 @@ module.exports={
                 loader: 'awesome-typescript-loader'
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        })
+    ]
 }
