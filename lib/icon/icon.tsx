e82ecdbfa32a6afa0ebaae04d2a48ece7ecb82de
韩佳宁 <hanjianing@mudu.tv>
2019-07-24 19:44:50 +0800

    icon 动态加载所有svg资源 另外多来点吃的

--- lib/icon/icon.tsx
+++ lib/icon/icon.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../icons/code.svg';
+import './importAllSvg.js';
 interface IconProps {
     name: string
 }
--- lib/index.tsx
+++ lib/index.tsx
@@ -4,4 +4,4 @@ import ReactDom from 'react-dom';
 import Icon from './icon/icon';
 
 
-ReactDom.render(<Icon name='sm'/>, document.getElementById('root'));
+ReactDom.render(<Icon name='code'/>, document.getElementById('root'));
