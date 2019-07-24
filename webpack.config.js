b65c38a188fa4e52935d9d7b03170f6cdb6d6ffe
韩佳宁 <hanjianing@mudu.tv>
2019-07-24 11:31:52 +0800

    jest ts babel 相关配置调试完毕

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,5 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports={
-    //mode: 'production',
-   // mode: 'development',
     entry:{
         index:'./lib/index.tsx'
     },
@@ -22,24 +19,4 @@ module.exports={
             }
         ]
     },
-    // plugins: [
-    //     new HtmlWebpackPlugin({
-    //         template: 'index.html'
-    //     })
-    // ],
-    // externals:{
-    //     react:{
-    //         // 这四个是对应兼容历史上各种各样的打包方案
-    //         commonjs:'react',
-    //         commonjs2:'react',
-    //         amd:'react',
-    //         root:'React',
-    //     },
-    //     'react-dom':{
-    //         commonjs:'react-dom',
-    //         commonjs2:'react-dom',
-    //         amd:'react-dom',
-    //         root:'ReactDom',
-    //     },
-    // }
-}
+};
