ade6cb80d63275d002ee7575aaa5972e9b7df691
韩佳宁 <hanjianing@mudu.tv>
2019-07-25 17:16:52 +0800

    参数获取全都放到函数参数里

--- lib/icon/icon.tsx
+++ lib/icon/icon.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import classNameTool from '../Tools/classNameTool';
 import './importAllSvg.js';
 import './icon.scss';
-interface IconProps extends React.SVGAttributes<SVGElement>  {
+
+interface IconProps extends React.SVGAttributes<SVGElement> {
     type: string;
     //onClick:(e:React.MouseEvent)=>void
     // 改为默认的鼠标事件处理函数
-    onClick:React.MouseEventHandler<SVGElement>
+    onClick: React.MouseEventHandler<SVGElement>
 }
+
 // 为了让 IconProps 能支持更多的事件
 // 需要 onTouch ...... 更多的处理函数
 // 但是二三十个东西都写在 IconProps 里会很蠢
@@ -21,17 +23,17 @@ interface IconProps extends React.SVGAttributes<SVGElement>  {
 // 还有一个 SVGAttributes 继承了 DOMAttributes
 // 显然 Icon 这个使用svg 的组件 需要 SVGAttributes
 // 所以 只需要让  IconProps  继承 SVGAttributes 就可以了
-const Icon:React.FunctionComponent<IconProps>=(props)=> {
-    const {className ,...restProps} = props
-    return (
-        <svg
-            //className={`unp-icon ${className}`}
-            className={classNameTool('unp-icon',className)}
-            {...restProps}
-        >
-            <use xlinkHref={`#${props.type}`}/>
-        </svg>)
-};
+const Icon: React.FunctionComponent<IconProps> =
+    ({className, type, ...restProps}) => {
+        return (
+            <svg
+                //className={`unp-icon ${className}`}
+                className={classNameTool('unp-icon', className)}
+                {...restProps}
+            >
+                <use xlinkHref={`#${type}`}/>
+            </svg>);
+    };
 
 export default Icon;
 
