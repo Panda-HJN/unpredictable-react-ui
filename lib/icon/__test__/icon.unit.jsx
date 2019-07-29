c38ce007f3960a369f07101c91dd1f15a467e05b
韩佳宁 <hanjianing@mudu.tv>
2019-07-29 10:41:47 +0800

    给icon 写测试

--- lib/icon/__test__/icon.unit.jsx
+++ lib/icon/__test__/icon.unit.jsx
@@ -1,18 +1,18 @@
-import * as renderer from 'react-test-renderer'
-import React from 'react'
-import Icon from '../icon'
-import {mount} from 'enzyme'
+import * as renderer from 'react-test-renderer';
+import React from 'react';
+import Icon from '../icon';
+import {mount} from 'enzyme';
 
 
-describe('icon 测试',()=>{
-    it('渲染成功', () => {
-        const json = renderer.create(<Icon name="code"/>).toJSON()
-        expect(json).toMatchSnapshot()
+describe('icon 的简单测试', () => {
+    it('渲染是否成功', () => {
+        const json = renderer.create(<Icon name="code"/>).toJSON();
+        expect(json).toMatchSnapshot();
     });
-    it('onClick', () => {
-        const fn = jest.fn()
-        const component = mount(<Icon name="code" onClick={fn}/>)
+    it('点击是否被响应', () => {
+        const fn = jest.fn();
+        const component = mount(<Icon name="code" onClick={fn}/>);
         component.find('svg').simulate('click');
-        expect(fn).toBeCalled()
-    })
+        expect(fn).toBeCalled();
+    });
 });
