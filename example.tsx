e68d74e71d9c61a1906396900c4b55e3a53976d1
韩佳宁 <hanjianing@mudu.tv>
2019-08-19 16:14:27 +0800

    修改涉及 ci 的相关逻辑, 添加 demo doc 等内容

---
+++ example.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {HashRouter as Router} from 'react-router-dom';
+
+import './example.scss';
+
+ReactDOM.render(
+  <Router>
+    <div>demo</div>
+  </Router>
+  , document.querySelector('#root'));
--- jest.config.ci.js
+++ jest.config.ci.js
@@ -1,8 +1,9 @@
-const base = require('./jest.config');
+const base = require('./jest.config')
 module.exports = Object.assign({}, base, {
-    reporters: ["default","jest-junit"],
+    reporters: ["jest-junit"],
     collectCoverage: true,
-    // collectCoverageFrom: ["lib/**/*.{ts,tsx}", "!**/node_modules/**"],
-    // coverageDirectory: 'coverage',
-    // coverageReporters: ['text', 'lcov'],
-});
+    collectCoverageFrom: ["lib/**/*.{ts,tsx}", "!**/node_modules/**"],
+    coverageDirectory: 'coverage',
+    coverageReporters: ['text', 'lcov'],
+})
+
