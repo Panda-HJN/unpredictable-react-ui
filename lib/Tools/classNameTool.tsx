93dee797e0caa88e87e78946059e3f7047f977f2
韩佳宁 <hanjianing@mudu.tv>
2019-08-21 19:49:57 +0800

    添加classname prefix 工具函数

--- lib/Tools/classNameTool.tsx
+++ lib/Tools/classNameTool.tsx
@@ -1,4 +1,10 @@
 function classNameTool (...names:(string|undefined)[]) {
     return names.filter(Boolean).join(' ')
 }
+function classNamePrefix (prefix:string) {
+    return function (name:string) {
+        return[prefix,name].filter(Boolean).join('-')
+    }
+}
+export  {classNamePrefix};
 export default classNameTool;
--- lib/modal/modal.tsx
+++ lib/modal/modal.tsx
@@ -1,25 +1,28 @@
 import React, {Fragment} from 'react';
+import {classNamePrefix} from '../Tools/classNameTool';
 import './style.scss';
+
 interface Props {
     visible: boolean;
 }
 
 const Modal: React.FunctionComponent<Props> = (props) => {
     const {visible,children} = props;
+    const c =classNamePrefix('und-modal')
     const onClickMask= ()=>{};
     return (visible ?
         <Fragment>
-            <div className="und-modal-mask" onClick={onClickMask}/>
+            <div className={c('mask')} onClick={onClickMask}/>
             <div className='und-modal'>
-                <header>
+                <header className={c('header')}>
                     title
                 </header>
                 {/*closeBtn不应该放进header里 放进去就暗示着 想有closeBtn就必须有header*/}
-                <div className="closeBtn">x</div>
-                <main>
+                <div className={c('close')}>x</div>
+                <main className={c('main')}>
                     {children}
                 </main>
-                <footer>
+                <footer className={c('footer')}>
                     <button>1</button>
                     <button>2</button>
                 </footer>
