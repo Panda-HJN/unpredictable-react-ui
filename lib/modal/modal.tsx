b7369a3b91ad0641fd8c6c6fdb8151bd797b9fba
韩佳宁 <hanjianing@mudu.tv>
2019-08-22 20:09:27 +0800

    简单的 modal

--- lib/modal/modal.tsx
+++ lib/modal/modal.tsx
@@ -1,24 +1,26 @@
 import React, {Fragment} from 'react';
+import {Icon} from '../index'
 import {classNamePrefix} from '../Tools/classNameTool';
 import './style.scss';
 
 interface Props {
     visible: boolean;
+    title?: string;
 }
 
 const Modal: React.FunctionComponent<Props> = (props) => {
-    const {visible,children} = props;
-    const c =classNamePrefix('und-modal')
+    const {visible,title,children} = props;
+    const c =classNamePrefix('und-modal');
     const onClickMask= ()=>{};
     return (visible ?
         <Fragment>
             <div className={c('mask')} onClick={onClickMask}/>
             <div className='und-modal'>
                 <header className={c('header')}>
-                    title
+                    {title}
+                    <div className={c('close-icon')}><Icon type='close'/></div>
                 </header>
                 {/*closeBtn不应该放进header里 放进去就暗示着 想有closeBtn就必须有header*/}
-                <div className={c('close')}>x</div>
                 <main className={c('main')}>
                     {children}
                 </main>
